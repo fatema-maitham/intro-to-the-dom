1fc315d120dfedd4ef2d547713a28e7f6528b280
fatema-maitham <fatemamaitham1@gmail.com>
2026-07-26 14:33:14 +0300

    done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
+// UI Cached Variables
 console.log("Hello, World!");
+
+const titleElement = document.querySelector('#main-title');
+const paragraphElement = document.querySelector('.cool')
+
+console.log(paragraphElement.textContent);
+
+paragraphElement.textContent = "I like pizza";
+paragraphElement.style.color = "red";
+paragraphElement.style.fontWeight = "bold";
+
+titleElement.style.color = "blue";
+titleElement.style.fontSize = "40px";
+titleElement.style.textAlign = "center";
+
+console.log(paragraphElement.textContent);
+
+console.dir(titleElement);
+console.dir(paragraphElement);
+
+const bodyElement = document.querySelector('body');
+console.dir(bodyElement);
+
+const h2Element = document.createElement('h2');
+h2Element.textContent = 'Comments section';
+
+bodyElement.appendChild(h2Element);
